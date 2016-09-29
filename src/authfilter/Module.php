2329c6b3bb5b3042fe1f00e76a782215ab87c28b
indigerd <indigerd@templatemonster.me>
2016-09-29 19:07:54 +0300

    fixed test helper

--- src/authfilter/Module.php
+++ src/authfilter/Module.php
@@ -117,7 +117,7 @@ class Module extends \yii\base\Module
     public function validateRequest(Request $request)
     {
         if ($this->testMode) {
-            return json_decode(TestHelper::getTokenInfo(), true);
+            return json_decode(TestHelper::getTokenInfo()->content, true);
         }
         $accessToken = $this->determineAccessToken($request);
         try {
